9dab19f9092b593d3dcb9064ca5fd91c965b0f78
jeonghyeon00 <sjh00kr@naver.com>
2024-10-07 12:34:44 +0900

    feat: add affiliations

--- src/main/kotlin/com/jeonghyeon00/commit/graph/infrastructure/github/GithubGraphQLClient.kt
+++ src/main/kotlin/com/jeonghyeon00/commit/graph/infrastructure/github/GithubGraphQLClient.kt
@@ -17,7 +17,7 @@ class GithubGraphQLClient(
             query User {
                 user(login: "$githubId") {
                     name
-                    repositories(isFork: false, first: 100) {
+                    repositories(isFork: false, first: 100, affiliations: OWNER, orderBy: { field: UPDATED_AT, direction: DESC }) {
                         nodes {
                             name
                             languages(first: 10, orderBy: { field: SIZE, direction: DESC }) {
